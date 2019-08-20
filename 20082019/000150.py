2dfcdbbc2c58e220dc2fefdfa3f5a4576055f299
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-20 12:20:41 -0400

    Create 000150.py

---
+++ 20082019/000150.py
@@ -0,0 +1,11 @@
+#minuto 1:50-5:05
+
+#a colelecction of instructions
+#a colelecction of code
+def function1():
+    print("ahhhhh")
+    print("ahhhh 2")
+print("this is outside the function")
+
+function1()
+function1()
