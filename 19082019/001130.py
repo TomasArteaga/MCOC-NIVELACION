31a1f9740ef5bc6ff8be9298779f90c62e4b5745
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-19 23:35:48 -0400

    Create 001130.py

---
+++ 19082019/001130.py
@@ -0,0 +1,38 @@
+#minuto 11:30-15:10
+e=7
+f=8
+if e<f:
+    print("e is less than f")
+elif e==f:
+    print("e is equal to f")
+else:
+    print("e is greater than f")
+e=8
+f=8
+if e<f:
+    print("e is less than f")
+elif e==f:
+    print("e is equal to f")
+else:
+    print("e is greater than f")
+e=10
+f=8
+if e<f:
+    print("e is less than f")
+elif e==f:
+    print("e is equal to f")
+else:
+    print("e is greater than f")
+
+e=20
+f=8
+if e<f:
+    print("e is less than f")
+elif e==f:
+    print("e is equal to f")
+elif e>f+10:
+    print("e is greater than f by more than 10")
+else:
+    print("e is greater than f")
+  
+    
