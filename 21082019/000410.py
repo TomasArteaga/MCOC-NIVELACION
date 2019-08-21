818864286802e8a6ec5d7a9cd94b2204b9fecf2c
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-21 17:31:38 -0400

    Create 000410.py

---
+++ 21082019/000410.py
@@ -0,0 +1,18 @@
+#0:20-1:20
+a=[3,10,-1]
+print (a)
+#1:20-3:10
+a.append(1)
+print(a)
+a.append("hello")
+print(a)
+a.append([1,2])
+print(a)
+#3:10-4:10
+a.pop()
+print(a)
+a.pop()
+print(a)
+#4:10-5:30
+print(a[0])
+print(a[3])
