196da18555fb6fc3c1e7c851392013925a9b0040
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-22 18:36:22 -0400

    Create 000320.py

---
+++ 22082019/000320.py
@@ -0,0 +1,6 @@
+minuto 3:20-4:50
+#r haciendo una range,se hace una lista con cierta condiciones, tomando el numero inicial y tomando el numero anterior del ultimo
+#1,2,3,4
+c=list(range(1,5))
+print(c)
+#minuto 4:50-6:00
