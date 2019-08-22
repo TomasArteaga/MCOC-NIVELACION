3dd174fe688926b578281533c536119d63e95c1c
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-22 18:34:59 -0400

    Create 000000.py

---
+++ 22082019/000000.py
@@ -0,0 +1,8 @@
+#minuto 0:00-1:47
+# como va corriendo los elemton en la lista
+
+a=["banana","apple","microsoft"]
+for element in a:
+    print(element)
+    #si impriminos 2 veces se ordena acorde de la posicion
+    print(element)
