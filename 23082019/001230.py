770e424d82e42d99f7f1abdf982492e9d032e005
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-23 15:20:31 -0400

    Create 001230.py

---
+++ 23082019/001230.py
@@ -0,0 +1,13 @@
+#minuto 12:30-13:18
+#vamos imprimir la suma de los numeros positivos 
+
+given_list3=[7,5,4,4,3,1,-2,-3,-5,-7] 
+total6=0
+i=0
+while given_list3[i]>0:
+    total6+=given_list3[i]
+    i+=1
+print(total6)
+
+        
+    
