fd63ee493c1217b83075331f91cdb4be7d844457
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-23 15:17:52 -0400

    Create 000255.py

---
+++ 23082019/000255.py
@@ -0,0 +1,9 @@
+ #minuto 2:55-6:10
+# se esta recorriendo un while, por lo que se estan sumando los numero de la lista mayores a 0
+given_list=[5,4,4,3,1,-2,-3,-5]
+total3=0
+i=0
+while given_list[i]>0:
+    total3 += given_list[i]
+    i+=1
+print(total3)   
