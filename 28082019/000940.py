2bdd03330724309c89078bef76f01c2ddf3b377d
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-28 23:45:17 -0400

    Create 000940.py

---
+++ 28082019/000940.py
@@ -0,0 +1,19 @@
+#minuto 9:40-15:45
+# diferente formas de arreglos
+#b=np.array(([1,5,2,3]),([4,5,6]))
+#print b
+#b=np.array(1,2,3)
+#print b
+#print a<4
+a=np.ones((2,3))
+print a
+a=np.ones(10)
+print a
+a=np.array([2,3,4], dtype=int16)
+print a.dtype
+
+
+a=np.random.random((2,3))
+print a
+a=np.random.randint(1,10,6)
+print a
