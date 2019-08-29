184aa13a3b6cda40e2d336d1463e9af835df834b
TomasArteaga <53574632+TomasArteaga@users.noreply.github.com>
2019-08-28 23:44:44 -0400

    Create 000550.py

---
+++ 28082019/000550.py
@@ -0,0 +1,14 @@
+#minuto 5:50-9:40
+#diferente codigo para entender las difernerte manera de arreglos qje hay, diferenrte tipos y usos
+import numpy as np
+a=np.array([2,3,4])
+print a
+
+a=np.array([1,12,2])
+print a
+a=np.linspace(1,12,6)
+print a.reshape(3,2)
+print a
+print a.shape
+print a.dtype
+print a.itemsize
